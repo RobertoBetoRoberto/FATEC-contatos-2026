02bbc26babf45df20e35e4c1a6aa13d42e4315f3
roberto.alves5@fatec.sp.gov.br <roberto.alves5@fatec.sp.gov.br>
2026-05-09 15:21:53 -0300

    Exclusão do contatos.js, leve correção no contato.js e alteração de nome para contatos.js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+'use strict'
+
+import { getContatos, deleteContato } from "./contato.js"
+
+getContatos
--- js/contatos.js
+++ js/contatos.js
@@ -1,59 +1,64 @@
-const BASE_URL = "https://bakcend-fecaf-render.onrender.com/contatos"
+const URL = 'https://bakcend-fecaf-render.onrender.com/contatos'
 
-export async function getContatos() {
-  const response = await fetch(BASE_URL)
-  if (!response.ok) {
-    throw new Error("Erro ao buscar contatos")
-  }
-  return response.json()
+export async function getContatos(){
+    const response = await fetch (URL)
+    if (!response.ok) throw new Error("Erro ao buscar contatos")
+    return response.json()
 }
 
-export async function criarContato(contato) {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(contato)
-  }
+export async function postContato(contato){
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(contato)
+    }
 
-  const response = await fetch(BASE_URL, options)
-
-  if (!response.ok) {
-    throw new Error("Erro ao criar contato")
-  }
-
-  return response.json()
+        const response = await fetch(URL, options)
+        if (!response.ok) throw new Error("Erro ao criar um contato")
+            
+        return response.json() 
 }
 
-export async function atualizarContato(id, contato) {
-  const options = {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(contato)
-  }
+export async function putContato(id, contato) {
+    const options = {
+        method: "PUT",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(contato)
+    }
 
-  const response = await fetch(`${BASE_URL}/${id}`, options)
+    const response = await fetch (`${URL}/${id}`, options)
 
-  if (!response.ok) {
-    throw new Error("Erro ao atualizar contato")
-  }
-
-  return response.json()
+    if (!response.ok) throw new Error("Erro ao atualizar o contato!")
+    
+    return response.json()
 }
 
-export async function deletarContato(id) {
-  const options = {
-    method: "DELETE"
-  }
+export async function deleteContato(id){
+    const options ={
+        method: "DELETE"
+    }
+    const response = await fetch (`${URL}/${id}`, options)
 
-  const response = await fetch(`${BASE_URL}/${id}`, options)
+    if (!response.ok) throw new Error("Erro ao deletar o contato!")
 
-  if (!response.ok) {
-    throw new Error("Erro ao deletar contato")
-  }
-
-  return true
+    return true
 }
+
+const novoContato = {
+  "nome": "Aqui é o Patrick",
+  "celular": "1",
+  "foto": "https://i.ytimg.com/vi/7GYegWy_Prk/maxresdefault.jpg",
+  "email": "SiriCascudo@email.com",
+  "endereco": "Rua do Siri Cascudo,  Nº 1",
+  "cidade": "Fenda do Bikini"
+}
+
+// postContato(novoContato)
+
+// putContato(26, novoContato)
+
+// deleteContato(26)
